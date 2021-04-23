4aa8c0c8f85e13389da273ccb66322f9572b9444
Alexandre Delattre <alexandre.delattre.biz@gmail.com>
2021-04-23 08:24:04 +0200

    Matcher macro

--- src/matchers.rs
+++ src/matchers.rs
@@ -1,42 +1,25 @@
 use glob::Pattern;
 use ocy_core::matcher::Matcher;
 
+macro_rules! matcher {
+    ($name: expr, $to_match: expr, $to_remove: expr) => {
+        Matcher::new(
+            $name.into(),
+            Pattern::new($to_match).unwrap(),
+            Pattern::new($to_remove).unwrap(),
+        )
+    };
+}
+
 pub fn standard_matchers() -> Vec<Matcher> {
     vec![
-        Matcher::new(
-            "Cargo".into(),
-            Pattern::new("Cargo.toml").unwrap(),
-            Pattern::new("target").unwrap(),
-        ),
-        Matcher::new(
-            "Gradle".into(),
-            Pattern::new("build.gradle").unwrap(),
-            Pattern::new("build").unwrap(),
-        ),
-        Matcher::new(
-            "Maven".into(),
-            Pattern::new("pom.xml").unwrap(),
-            Pattern::new("target").unwrap(),
-        ),
-        Matcher::new(
-            "NodeJS".into(),
-            Pattern::new("*").unwrap(),
-            Pattern::new("node_modules").unwrap(),
-        ),
-        Matcher::new(
-            "XCode".into(),
-            Pattern::new("*").unwrap(),
-            Pattern::new("DerivedData").unwrap(),
-        ),
-        Matcher::new(
-            "SBT".into(),
-            Pattern::new("build.sbt").unwrap(),
-            Pattern::new("target").unwrap(),
-        ),
-        Matcher::new(
-            "SBT".into(),
-            Pattern::new("plugins.sbt").unwrap(),
-            Pattern::new("target").unwrap(),
-        ),
+        matcher!("Cargo", "Cargo.toml", "target"),
+        matcher!("Gradle", "build.gradle", "build"),
+        matcher!("GradleKTS", "build.gradle.kts", "build"),
+        matcher!("Maven", "pom.xml", "target"),
+        matcher!("NodeJS", "*", "node_modules"),
+        matcher!("XCode", "*", "DerivedData"),
+        matcher!("SBT", "build.sbt", "target"),
+        matcher!("SBT", "plugins.sbt", "target"),
     ]
 }
